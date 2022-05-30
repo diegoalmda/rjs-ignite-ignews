8ff5d886c57d4914c33d617ce29532b8cea3be80
Diego Almeida <diegoalmda@gmail.com>
2022-05-30 15:20:48 -0300

    Code refactor

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -22,71 +22,72 @@ export default NextAuth({
   // },
   callbacks: {
     async session({session, user, token}) {
-      try {
-        const userActiveSubscription = await fauna.query(
-          q.Get(
-            q.Intersection([
-              q.Match(
-                q.Index('subscription_by_user_ref'),
-                q.Select(
-                  "ref",
-                  q.Get(
-                    q.Match(
-                      q.Index('user_by_email'),
-                      q.Casefold(session.user.email)
-                    )
-                  )
-                )
-              ),
-              q.Match(
-                q.Index('subscription_by_status'),
-                "active"
-              )
-            ])
-          )
-        )
-  
-        return {
-          ...session,
-          activeSubscription: userActiveSubscription
-        }
 
-      } catch {
-        return {
-          ...session,
-          activeSubscription: null
-        }
-      }
-    },
-    async signIn({ user, account, profile, email, credentials }) {
-
-      try {
-        await fauna.query(
-          q.If(
-            q.Not(
-              q.Exists(
-                q.Match(
-                  q.Index('user_by_email'),
-                  q.Casefold(email)
+        try {
+            const userActiveSubscription = await fauna.query(
+                q.Get(
+                    q.Intersection([
+                        q.Match(
+                            q.Index('subscription_by_user_ref'),
+                            q.Select(
+                                'ref',
+                                q.Get(
+                                    q.Match(
+                                        q.Index('user_by_email'),
+                                        q.Casefold(session.user.email)
+                                    )
+                                )
+                            )
+                        ),
+                        q.Match(
+                            q.Index('subscription_by_status'),
+                            'active'
+                        )
+                    ])
                 )
-              )
-            ),
-            q.Create(
-              q.Collection('users'),
-              { data: { email } }
-            ),
-            q.Get(
-              q.Match(
-                q.Index('user_by_email'),
-                q.Casefold(email)
-              )
             )
-          )
-        )
-        return true
-      } catch {
-        return false
-      }
+            return {
+                ...session,
+                activeSubscription: userActiveSubscription
+            }
+        } catch {
+            return {
+                ...session,
+                activeSubscription: null
+            }
+        }
     },
-  }
+    async signIn({ user, account, profile }) {
+        const { email } = user; // email que vem de dentro de user;
+
+        try {
+            await fauna.query(
+                q.If( // SE
+                    q.Not( // NÃO
+                        q.Exists( // EXISTE
+                            q.Match(
+                                q.Index('user_by_email'), // procurar por indexe criado no Fauna: user_by_email;
+                                q.Casefold(user.email) // deixa a filtragem igual sendo uppercase ou não;
+                            )
+                        )
+                    ),
+                    q.Create(
+                        q.Collection('users'), // nome da tabela;
+                        { data: { email } } // dados do usuario que queremos inserir;
+                    ),
+                    q.Get( // Selecionar usuario que bate com index abaixo:
+                        q.Match(
+                            q.Index('user_by_email'),
+                            q.Casefold(user.email)
+                        )
+                    )
+                )
+            )
+            return true
+
+        } catch {
+            return false;
+        }
+    },
+},
 })
