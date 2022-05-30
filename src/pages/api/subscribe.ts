701853615107411853fa5474091e57c24576da8c
Diego Almeida <diegoalmda@gmail.com>
2022-05-30 15:45:04 -0300

    Code refactor

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,71 +1,71 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { query as q } from 'faunadb';
-import { getSession } from "next-auth/react";
-import { stripe } from "../../services/stripe";
+import { getSession } from 'next-auth/react'
 import { fauna } from "../../services/fauna";
+import { stripe } from "../../services/stripe";
 
 type User = {
-    ref: {
-        id: string
-    }
-    data: {
-        stripe_customer_id: string
-    }
+  ref: {
+    id: string;
+  }
+  data: {
+    stripe_customer_id: string;
+  }
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse)=> {
-    if(req.method === 'POST') { // verificar se o metodo é post, pois estamos criando algo no nosso bd;
-        const session = await getSession({ req }) // dentro de req temos os coockies
+export default async function subscribe(req: NextApiRequest, res: NextApiResponse) {
+  if(req.method === 'POST') {
+    const session = await getSession({ req })
 
-        const user = await fauna.query<User>( // Primeiro devemos procurar um usuario por email;
-            q.Get(
-                q.Match(
-                    q.Index('user_by_email'),
-                    q.Casefold(session.user.email)
-                )
-            )
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
         )
+      )
+    )
 
-        let customerId = user.data.stripe_customer_id;
+    let customerId = user.data.stripe_customer_id
 
-        if (!customerId) { // se id de novo customer não existir ele:
-            const stripeCustomer = await stripe.customers.create({ // Cria um customer;
-                email: session.user.email,
-            });
+    if(!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // medatada
+      })
 
-            await fauna.query( // salva no banco;
-                q.Update( 
-                    q.Ref(q.Collection('users'), user.ref.id),
-                    {
-                        data: {
-                            stripe_customer_id: stripeCustomer.id
-                        }
-                    }
-                )
-            )
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
 
-            customerId = stripeCustomer.id; // reatribui a variavel;
-        }
+      customerId = stripeCustomer.id
+
+    }    
 
 
-        const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: customerId, // quem está comprando o produto! ID do nosso cliente do stripe
-            payment_method_types: ['card'],
-            billing_address_collection: 'required',
-            line_items: [
-                { price: 'price_1Kyle2FnH6B3doFuKsgFT0kN', quantity: 1 }
-            ],
-            mode: 'subscription',
-            allow_promotion_codes: true,
-            success_url: process.env.STRIPE_SUCCESS_URL,
-            cancel_url: process.env.STRIPE_CANCEL_URL
-        })
-        return res.status(200).json({sessionId: stripeCheckoutSession.id}); // deu certo
-    } else {
-        res.setHeader('Allow',  'POST');
-        res.status(405).end('Method not allowed');
-    }
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: 'price_1Kyle2FnH6B3doFuKsgFT0kN', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
 }
-
-
-
